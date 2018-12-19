a4d5ffced50d818267fa96e8a9065b0585626620
deatheyes <deatheye@yeah.net>
2018-12-19 13:00:50 +0800

    update: sed path

--- dependency/sqltypes/value.go
+++ dependency/sqltypes/value.go
@@ -23,10 +23,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/xwb1989/sqlparser/dependency/bytes2"
-	"github.com/xwb1989/sqlparser/dependency/hack"
+	"github.com/deatheyes/sqlparser/dependency/bytes2"
+	"github.com/deatheyes/sqlparser/dependency/hack"
 
-	"github.com/xwb1989/sqlparser/dependency/querypb"
+	"github.com/deatheyes/sqlparser/dependency/querypb"
 )
 
 var (
--- token.go
+++ token.go
@@ -22,8 +22,8 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/xwb1989/sqlparser/dependency/bytes2"
-	"github.com/xwb1989/sqlparser/dependency/sqltypes"
+	"github.com/deatheyes/sqlparser/dependency/bytes2"
+	"github.com/deatheyes/sqlparser/dependency/sqltypes"
 )
 
 const (
